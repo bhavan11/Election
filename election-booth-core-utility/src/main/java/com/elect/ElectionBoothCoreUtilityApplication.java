0120365b17c370b41caf9db72f99b57797c2aa02
bhavan_11 <bhavan.patel015@gmail.com>
2022-11-15 20:46:53 +0530

    Stable Commit

---
+++ election-booth-core-utility/src/main/java/com/elect/ElectionBoothCoreUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.elect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionBoothCoreUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionBoothCoreUtilityApplication.class, args);
+	}
+
+}
--- election-booth-share/src/main/java/com/elect/rest/ElectionBoothController.java
+++ election-booth-share/src/main/java/com/elect/rest/ElectionBoothController.java
@@ -1,22 +1,33 @@
 package com.elect.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ElectionBoothController {
 
+    @Autowired
+    @Qualifier("ElectionBoothWhatsappImpl")
+    ElectionBoothInterface electionBoothWhatsapp;
     @GetMapping("/demoApi")
     public String getDemoString(){
+
         return "Success";
     }
 
-    @GetMapping("/sendSms")
+    @GetMapping("/whatsapp")
+    public List<String> sendWhatsappNotification(){
+        return Collections.singletonList(electionBoothWhatsapp.notifyUserWithBoothInfo());
+    }
+
+    @GetMapping("/sms")
     public List<String> sendSMS(){
         return null;
     }
